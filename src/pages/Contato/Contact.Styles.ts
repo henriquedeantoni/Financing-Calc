4e7b93bd44f7d30e1c4ad6b6e6e1426cf67a39e2
Henrique de Antoni <henriqdeantoni@gmail.com>
2025-01-12 16:29:44 -0300

    feat: criado rotas e sua abstracao

---
+++ src/pages/Contato/Contact.Styles.ts
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+
+export const Layout = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+`;
+
+export const MainWrapper = styled.div`
+  display: flex;
+  gap: 20px;
+`;
