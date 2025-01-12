abb188658c7b8ca2e3e00b3ce9900e40c1f47e8f
Henrique de Antoni <henriqdeantoni@gmail.com>
2025-01-12 02:00:57 -0300

    Adicionado Header a pagina

---
+++ src/components/atoms/Logo/Logo.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const LogoWrapper = styled.div`
+  font-size: 24px;
+  font-weight: bold;
+`;
+
+export const Logo = () => <LogoWrapper>Logo</LogoWrapper>;
